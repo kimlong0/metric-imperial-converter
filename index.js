fb81367215ece632930f1bb486999fc29c4ff941
kimlong0 <kimlong0@uw.edu>
2022-08-01 00:46:56 -0700

    fix conversion and display elements

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ let volume = document.getElementById("volume");
 let mass = document.getElementById("mass")
 
 //conversions
-const meter = (inputEl.value / 3.281).toFixed(3);
-const feet = (inputEl.value * 3.281).toFixed(3);
-const liter = (inputEl.value / 0.264).toFixed(3);
-const gallons = (inputEl.value * 0.264).toFixed(3);
-const kilo = (inputEl.value / 2.204).toFixed(3);
-const pounds = (inputEl.value * 2.204).toFixed(3);
+let meter = 0;
+let feet = 0;
+let liter = 0;
+let gallons = 0;
+let kilo = 0;
+let pounds = 0;
 
 convertBtn.addEventListener("click", function() {
     renderValues();
@@ -23,14 +23,20 @@ inputEl.addEventListener("keyup", function(press) {
 })
 
 function renderValues() {
-    renderConvert(length, meter, feet, "meters", "feet");
-    renderConvert(volume, liter, gallons, "liters", "gallons");
-    renderConvert(mass, kilo, pounds, "kilos", "pounds");
+    meter = (inputEl.value / 3.281).toFixed(3);
+    feet = (inputEl.value * 3.281).toFixed(3);
+    liter = (inputEl.value / 0.264).toFixed(3);
+    gallons = (inputEl.value * 0.264).toFixed(3);
+    kilo = (inputEl.value / 2.204).toFixed(3);
+    pounds = (inputEl.value * 2.204).toFixed(3);
+    renderSection(length, meter, feet, "meters", "feet");
+    renderSection(volume, liter, gallons, "liters", "gallons");
+    renderSection(mass, kilo, pounds, "kilos", "pounds");
     inputEl.placeholder = inputEl.value;
     inputEl.value = '';
 }
 
-function renderConvert(section, metricNum, imperialNum, metric, imperial) {
+function renderSection(section, metricNum, imperialNum, metric, imperial) {
     if (inputEl.value == false) {
         inputEl.value = 0;
     }
